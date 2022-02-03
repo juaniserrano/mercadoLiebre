e605170b867d85a175718b76a4681e2a85715124
juaniserrano <juanignacioserranoluna@gmail.com>
2022-02-03 19:09:29 -0300

    se agrega heroku al port de app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(process.end.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor funcionando');
 });
 
